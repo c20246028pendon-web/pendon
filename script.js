85754ece3784bd635e2552b4345f9d6421fa3aad
mansanas <c20246028.pendon@csav.edu.ph>
2026-09-04 16:37:00 +0800

    querySelector

--- script.js
+++ script.js
@@ -12,11 +12,18 @@ console.log(`address: ${address}`);
 console.log(67); 
 
 
-function greet (greetings, names = []) {
-    if (Array.isArray (names)) {
-    names = names, join(", ");
-}
-return `${greetings} sa enyu ${names}`;
+function greet (greetings, name = []){
+    if (Array.isArray(name)) {
+        name = name. join(", ");
+return `${greetings} sa enyu ${name}`;
 }
 console.log (greet("mayung aga", ["REY",  "Bai", "Soy", "Tol", "Erp",]));
+}
+ const heading = document.querySelector("h1");
+ console.log (heading);
 
+ const contactHeading = document. querySelector ("#contact h2");
+ console.log(contactHeading);
+
+ const servicesHeading = document.querySelector ("#services h2");
+ console.log(servicesHeading);   
