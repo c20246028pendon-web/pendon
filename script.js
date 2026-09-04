be12555d97c49a9a046739d3a7fc718828d9c107
mansanas <c20246028.pendon@csav.edu.ph>
2026-09-04 17:41:19 +0800

    addEventListener

--- script.js
+++ script.js
@@ -21,14 +21,35 @@ console.log (greet("mayung aga", ["REY",  "Bai", "Soy", "Tol", "Erp",]));
 }
 
  const  heading= document.querySelector("h1");
- heading.textContent = "RONILO";
+ heading.textContent = "RONILO PENDON";
  heading.style.color = "purple";
  heading.style.backgroundColor = "yellow";
  heading.style.fontSize = "100px";
  console.log (heading);
+
+ heading.addEventListener("click", function () {
+heading.style.color = "red";
+console.log ("Heading was clicked.");
+ });
 
  const contactHeading = document. querySelector ("#contact h2");
  console.log(contactHeading);
 
  const projectHeading = document.querySelector ("#services h2");
  console.log(projectHeading);   
+
+ const toggleButton = document.querySelector ('#switch');
+ const body = document.querySelector ('body');
+ let isOff = false;
+
+ toggleButton.addEventListener ("click", function () {
+    isOff = !isOff;
+    
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor ="white";
+        body.style.color = "black"
+    }
+ })
